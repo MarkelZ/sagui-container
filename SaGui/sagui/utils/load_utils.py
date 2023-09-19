7a93e900454a9777d04ac1a30ceb501ef90eb001
mzubia <markel.xubi@gmail.com>
2023-09-19 19:47:33 +0200

    Add functionality to plot the trajectory traced by an agent

--- SaGui/sagui/utils/load_utils.py
+++ SaGui/sagui/utils/load_utils.py
@@ -1,30 +1,34 @@
+#!/usr/bin/env python
+
+import joblib
+import os
 import os.path as osp
 import tensorflow as tf
 from sagui.utils.logx import restore_tf_graph
 
-def load_policy_transfer(fpath, itr='last'):
+def load_policy(fpath, itr='last', deterministic=False):
 
     # handle which epoch to load from
     if itr=='last':
-        # saves = [int(x[11:]) for x in os.listdir(fpath) if 'simple_save' in x and len(x)>11]
-        saves = [int(x[11:]) for x in os.listdir(fpath) if 'tf1_save' in x and len(x)>11]
+        saves = [int(x[11:]) for x in os.listdir(fpath) if 'simple_save' in x and len(x)>11]
         itr = '%d'%max(saves) if len(saves) > 0 else ''
     else:
         itr = '%d'%itr
 
     # load the things!
     sess = tf.Session(graph=tf.Graph())
-
-    # model = restore_tf_graph(sess, osp.join(fpath, 'simple_save'+itr))
-    model = restore_tf_graph(sess, osp.join(fpath, 'tf1_save'+itr))
+    model = restore_tf_graph(sess, osp.join(fpath, 'simple_save'+itr))
 
     # get the correct op for executing actions
-    
-    log_action_op = model['logp_a']
-    action_op = model['pi']
+    if deterministic and 'mu' in model.keys():
+        # 'deterministic' is only a valid option for SAC policies
+        print('Using deterministic action op.')
+        action_op = model['mu']
+    else:
+        print('Using default action op.')
+        action_op = model['pi']
 
     # make function for producing an action given a single state
-    get_logp_a = lambda x, a : sess.run(log_action_op, feed_dict={model['x']: x[None,:], model['a']: a[None,:]})[0]
     get_action = lambda x : sess.run(action_op, feed_dict={model['x']: x[None,:]})[0]
 
     # try to load environment from save
@@ -35,4 +39,4 @@ def load_policy_transfer(fpath, itr='last'):
     except:
         env = None
 
-    return env, get_logp_a, get_action, sess
+    return env, get_action, sess
