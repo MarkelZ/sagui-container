46599bfc6de972eeebc21ffd7e35cd721881fd62
mzubia <markel.xubi@gmail.com>
2023-10-22 20:20:01 +0000

    Use multithreading to speed up robustness evaluation

--- SaGui/sagui/eval-robust.py
+++ SaGui/sagui/eval-robust.py
@@ -1,62 +1,123 @@
 #!/usr/bin/env python
+import os
+from typing import Callable
 import numpy as np
 from sagui.utils.load_utils import load_policy
 from safety_gym.envs.engine import Engine
-from safety_gym.envs.world import World, Robot
+from multiprocessing.dummy import Pool
+from multiprocessing.pool import ThreadPool
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 
-def eval_cost(num_iters=20):
+
+def modify_constants(env: Engine, coef_dic: dict):
+    model = env.model
+    for coef, val in coef_dic.items():
+        atr = getattr(model, coef)
+        for index, _ in np.ndenumerate(atr):
+            atr[index] = val
+
+
+def eval_robust(n, coefs: dict, env: Engine, get_action: Callable[[np.ndarray], np.ndarray]):
     accum_cost = 0
-
-    for i in range(num_iters):
-        print('Trajectory ' + str(i))
-        o, r, d, ep_ret, ep_cost, ep_len, ep_goals, = env.reset(), 0, False, 0, 0, 0, 0
+    for _ in range(n):
+        o, d, ep_cost = env.reset(), False, 0
+        modify_constants(env, coefs)
         while not d:
             a = get_action(o)
-            o, r, d, info = env.step(a)
-            ep_ret += r
+            o, _, d, info = env.step(a)
             ep_cost += info.get('cost', 0)
-            ep_len += 1
-            ep_goals += 1 if info.get('goal_met', False) else 0
 
         accum_cost += ep_cost
 
-    return accum_cost / num_iters
+    return accum_cost / n
 
 
-# Type hints
-env: Engine
-robot: Robot
+def eval_coefs_robust(coef_list: list):
+    env, get_action, _ = load_policy('data/', itr=4, deterministic=True)
 
-# Load model and environment
-env, get_action, _ = load_policy('data/', itr=4, deterministic=True)
+    res = []
+    for coefs in coef_list:
+        print(f'Evaluating:\n{coefs}')
+        cost = eval_robust(100, coefs, env, get_action)
+        v = (coefs, cost)
+        res.append(v)
 
-robot = env.robot
-sim = robot.sim
-model = sim.model
+    return res
 
-print()
-print(sim.model.body_mass)        # Mass
-print(sim.model.dof_frictionloss) # Degree of freedom friction loss
-print(sim.model.geom_friction)    # Geometry friction loss
 
-n = len(sim.model.body_mass)
-for i in range(n):
-    sim.model.body_mass[i] *= 100
+def plot_trajectories(n):
+    # Load from save
+    env, get_action, _ = load_policy('data/', itr=4, deterministic=True)
 
-n = len(sim.model.dof_frictionloss)
-for i in range(n):
-    sim.model.dof_frictionloss[i] *= 0
+    # Plot trajectories
+    for i in range(n):
+        print(f'Trajectory {i}')
+        o, d, ep_cost = env.reset(), False, 0, 
+        positions = [env.robot_pos]
+        while not d:
+            a = get_action(o)
+            o, _, d, info = env.step(a)
+            ep_cost += info.get('cost', 0)
+            positions.append(env.robot_pos)
 
-n = len(sim.model.geom_friction)
-for i in range(n):
-    m = len(sim.model.geom_friction[i])
-    for j in range(m):
-        sim.model.geom_friction[i, j] *= 0
+        positions = np.array(positions)
+        x_positions = positions[:, 0]
+        y_positions = positions[:, 1]
 
-print()
-print(sim.model.body_mass)        # Mass
-print(sim.model.dof_frictionloss) # Degree of freedom friction loss
-print(sim.model.geom_friction)    # Geometry friction loss
+        color = 'blue' if ep_cost == 0 else 'black'
+        plt.plot(x_positions, y_positions, color=color)
 
-print(eval_cost())
+    # Add dummy trajectories for the legend
+    plt.plot([], [], color='blue', label='Safe')
+    plt.plot([], [], color='black', label='Unsafe')
+
+    # Draw hazard
+    hazard_circle = Circle((0, 0), 0.7, color='red', label='Hazard')
+    plt.gca().add_patch(hazard_circle)
+
+    # Plot settings
+    plt.xlabel('X Position')
+    plt.ylabel('Y Position')
+    plt.title('Robot Trajectories')
+    plt.legend()
+    plt.grid()
+
+    # Save plot
+    plt.savefig('./plot.png')
+
+
+# First, plot trajectories to make sure everything works
+plot_trajectories(100)
+
+# Number of processes
+num_procs = 10
+
+# Create a list of coefficients
+coef_list = []
+for mass in np.arange(start=0, stop=0.02, step=0.002):
+    for fric in np.arange(start=0, stop=0.01, step=0.001):
+        coef_dic = {'body_mass' : mass, 'dof_frictionloss' : fric}
+        coef_list.append(coef_dic)
+
+# Split the list of coefficients into equal chunks
+coef_list = np.array(coef_list)
+coef_sublists = np.array_split(coef_list, num_procs)
+
+# Create a thread pool and compute the robustness values
+pool: ThreadPool = Pool(num_procs)
+print(type(pool))
+results = pool.map(eval_coefs_robust, coef_sublists)
+
+# Close the pool and join the threads
+pool.close()
+pool.join()
+
+# Flatten the results and turn them into a string
+res_flat = [x for r in results for x in r]
+res_str = '[\n' + ',\n'.join(res_flat) + '\n]'
+
+# Save the results in a text file
+with open('./robust_results.txt', 'w') as f:
+    f.write(res_str)
